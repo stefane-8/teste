13a4ae54070c4fc1a911044bc19aba54fd6b4c70
Stefane-7 <249182153+stefane-8@users.noreply.github.com>
2025-12-12 21:24:08 -0300

    feat: estruturando gestão de contas basico

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    TIPOS_USUARIO = [
+        ('diretoria', 'Diretoria'),
+        ('associado', 'Associado'),
+        ('afiliado', 'Afiliado'),
+    ]
+
+    tipo_usuario = models.CharField(
+        max_length=20,
+        choices=TIPOS_USUARIO,
+        default='afiliado'
+    )
+
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+
